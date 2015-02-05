fbcad14a7d65655beb33d7f4869cbe05f1a2d393
friesey <y.friese@zbw.eu>
2015-02-05 15:59:37 +0100

    try sth with JHOVE2

---
+++ jhoveValidations/src/fileformats/tiff/Jhove2ValidatorTestTiff.java
@@ -0,0 +1,68 @@
+package fileformats.tiff;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import org.jhove2.core.Document;
+import org.jhove2.core.JHOVE2;
+import org.jhove2.core.io.Input;
+import org.jhove2.core.source.Source;
+import org.jhove2.core.source.SourceFactory;
+
+public class Jhove2ValidatorTestTiff {
+
+	static String folder;
+
+	public static void JhoveTiffValidator() {
+		String pathwriter;
+		try {
+			folder = starterDialogs.JhoveGuiStarterDialog.jhoveExaminationFolder;
+			if (folder != null) {
+
+				String xmlVersion = "xml version='1.0'";
+				String xmlEncoding = "encoding='ISO-8859-1'";
+
+				pathwriter = (folder + "//" + "Jhove2Exam.xml");
+				PrintWriter writer = new PrintWriter(new FileWriter(pathwriter));
+
+				writer.println("<?" + xmlVersion + " " + xmlEncoding + "?>");
+				writer.println("<JhoveFindings>");
+
+				ArrayList<File> files = validatorUtilities.ListsFiles.getPaths(new File(folder), new ArrayList<File>());
+
+				JHOVE2 JHOVE2 = new JHOVE2();
+
+				for (int i = 0; i < files.size(); i++) {
+					if (validatorUtilities.GenericFileAnalysis.testFileHeaderTiff(files.get(i)) == true) {
+
+						// Source source = SourceFactory.getSource(JHOVE2,
+						// files.get(i).toString());
+
+						Source source = JHOVE2.getSourceFactory().getSource(JHOVE2, files.get(i).toString());
+						source.addModule(JHOVE2);
+						Input input = source.getInput(JHOVE2);
+						JHOVE2.characterize(source, input);
+
+						/*
+						 * String title = "title"; Document.Type type =
+						 * Document.Type.getType(); Document.Intention intention
+						 * = "int";
+						 * 
+						 * Document tiffdoc = new Document(title, type,
+						 * intention);
+						 */
+
+					}
+				}
+				writer.close();
+			}
+
+		}
+
+		catch (Exception e) {
+
+		}
+	}
+}
